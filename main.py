e23374450eb145190906658ae279087c6eb5aa42
Sofia <msofia.lima@alu.ufc.br>
2024-07-25 09:31:53 -0300

    loading board

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 700, 700
 BG_COLOR = (230, 172, 182)
+BOARD = pygame.image.load('assets/Board.png')
+BOARD = pygame.transform.scale(BOARD, (WIDTH-100, HEIGHT-100))
+X = pygame.image.load('assets/X.png')
+O = pygame.image.load('assets/O.png')
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN.fill((BG_COLOR))
+WIN.blit(BOARD, (50, 50))
 pygame.display.set_caption("First Game")
 
 run = True
@@ -14,7 +20,6 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    WIN.fill((BG_COLOR))
     pygame.display.update()
 
 pygame.quit()
