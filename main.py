062ea532a4163d83959b8108cbceaae6cb591af3
Sofia <msofia.lima@alu.ufc.br>
2024-07-25 20:44:56 -0300

    added winning effect + move validation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 pygame.init()
 
 WIDTH, HEIGHT = 700, 700
-BG_COLOR = (230, 172, 182)
+BG_COLOR = (230, 214, 207)
 #loading assets
 BOARD = pygame.image.load('assets/newboard.png')
 X = pygame.image.load('assets/newx.png')
@@ -12,7 +12,9 @@ X = pygame.transform.scale(X, (150, 150))
 O = pygame.image.load('assets/newo.png')
 O = pygame.transform.scale(O, (150, 150))
 X_WIN = pygame.image.load('assets/xwin.png')
+X_WIN = pygame.transform.scale(X_WIN, (150, 150))
 O_WIN = pygame.image.load('assets/owin.png')
+O_WIN = pygame.transform.scale(O_WIN, (150, 150))
 #creating the window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 WIN.fill((BG_COLOR))
@@ -54,7 +56,8 @@ def findY(ypos):
     else:
         return 2
     
-def pcMove(board):
+def pcMove(board, valid):
+    if(not valid): return board
     x = random.randint(0, 2)
     y = random.randint(0, 2)
     while board[x][y] != -1:
@@ -70,12 +73,14 @@ def pcMove(board):
     return board
     
 def addXO(board, graphical_board, player):
+    valid = False
     #gets the mouse position
     current_pos = pygame.mouse.get_pos()
     xpos, ypos = current_pos
     #checks the board
     if(board[findX(xpos)][findY(ypos)]==-1):
         board[findX(xpos)][findY(ypos)] = player
+        valid = True
      #   board[round(converted_x), round(converted_y)] = player
     renderBoard(board, X, O)
 
@@ -85,28 +90,37 @@ def addXO(board, graphical_board, player):
                 WIN.blit(graphical_board[i][j][0], graphical_board[i][j][1])
 
     if((checkWinner(board) == None) and (not checkDraw(board))):
-        board = pcMove(board)
+        board = pcMove(board, valid)
 
     return board, player
 
 def checkWinner(board):
+    winner = None
     # Check rows
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] and board[i][0] != -1:
-            return board[i][0]
+            winner = board[i][0]
 
     # Check columns
     for i in range(3):
         if board[0][i] == board[1][i] == board[2][i] and board[0][i] != -1:
-            return board[0][i]
+            winner = board[0][i]
 
     # Check diagonals
     if board[0][0] == board[1][1] == board[2][2] and board[0][0] != -1:
-        return board[0][0]
+        winner = board[0][0]
 
     if board[0][2] == board[1][1] == board[2][0] and board[0][2] != -1:
-        return board[0][2]
-
+        winner = board[0][2]
+    
+    if(winner != None):
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == winner:
+                    graphical_board[i][j][0] = X_WIN
+                    WIN.blit(graphical_board[i][j][0], graphical_board[i][j][1])
+        pygame.display.update()
+        return winner
     return None
 
 def checkDraw(board):
